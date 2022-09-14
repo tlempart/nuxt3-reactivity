a01d60813f4d6b27c396d6c445acb5db1c96a869
Tomasz Lempart <tlempart@gmail.com>
2022-09-14 20:54:12 +0200

    reactivity sample

---
+++ app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div :class="divClass">Reactivity</div>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+import { useDisplay } from 'vuetify'
+
+const counter = ref(1)
+const { lgAndUp } = useDisplay()
+
+const divClass = computed(() => {
+  const result = lgAndUp.value ? 'green' : 'red'
+  console.log('result', result)
+
+  return result
+})
+</script>
+
+<style>
+  .green {
+    background-color: green;
+  }
+  .red {
+    background-color: red;
+  }
+</style>
